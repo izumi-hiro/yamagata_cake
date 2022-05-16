cfbad4547c036e7099748d0bed5dd50653268188
izumi-hiro <transformers2728frost@gmail.com>
2022-05-16 07:21:30 +0000

    admin/items/new

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  has_many :cart_items, dependent: :destory
+  has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.active_job.queue_adapter = :inline
+
+  config.hosts << "af346edd6b0c4628b4c2c28834ee122d.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
