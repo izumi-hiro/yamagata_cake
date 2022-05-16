2da23421bc8ce2f99f62dea3bca3b3d2624354a6
izumi-hiro <transformers2728frost@gmail.com>
2022-05-16 08:18:28 +0000

    [Add] views/admin/items/show

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,9 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
+  
+  def tax_included_price
+    (tax_excluded_price * 1.1).floor
+  end
 
 end
