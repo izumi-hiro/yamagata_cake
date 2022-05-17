46b0bd64c13649b541e8946af8665679fe9b4600
izumi-hiro <transformers2728frost@gmail.com>
2022-05-17 06:20:13 +0000

    [Add] admin_top_routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,14 @@ Rails.application.routes.draw do
 }
 
   namespace :admin do
+    root to: 'homes#top'
     resources :items, except: [:destroy]
   end
   
   scope module: :public do
     resources :items, only: [:index, :show]
+    root to: 'homes#top'
+    get '/about' => 'homes#about', as: 'about'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
